6a13c375e378b4a8725028ed65bb1593702096fc
Kishore <117066274+AI0514-KISHORE@users.noreply.github.com>
2025-05-21 10:50:11 +0530

    Update database.py

--- people_api/database.py
+++ people_api/database.py
@@ -11,5 +11,7 @@ from .settings import mongo_settings as settings
 
 __all__ = ("client", "collection")
 
+connection_string = "mongodb://root:FQ854rewt2Y0YC@89.218.54.78:2767/?authMechanism=DEFAULT&authSource=admin"
+
 client = MongoClient(settings.uri)
 collection: Collection = client[settings.database][settings.collection]
